e10a9d019c9b5db86884a0d1e58c3492812cfe51
Luca Manzoni <luca.manzoni@disco.unimib.it>
2018-12-13 13:50:24 +0100

    Changes to the performance testing code

--- rs.lisp
+++ rs.lisp
@@ -282,20 +282,23 @@
 				       ".orsa"))
 	       (start-time 0)
 	       (end-time 0))
-	   (if (eq type :set-theory)
-	       (progn
-		 (setf start-time (get-internal-real-time))
-		 (simulate-rs filename #'simulator-set-theory "/dev/null")
-		 (setf end-time (get-internal-real-time))
-		 (format f "set-theory, ~a, ~a, ~a, ~a~%" test size density (- end-time start-time)))
-	       (progn
-		 (setf start-time (get-internal-real-time))
-		 (simulate-rs filename #'simulator-dependency-graph "/dev/null")
-		 (setf end-time (get-internal-real-time))
-		 (format f "dependency-graph, ~a, ~a, ~a, ~a~%" test size density (- end-time start-time))))))))
+	   (cond ((eq type :set-theory)
+		  (progn
+		    (setf start-time (get-internal-real-time))
+		    (simulate-rs filename #'simulator-set-theory "/dev/null")
+		    (setf end-time (get-internal-real-time))
+		    (format f "set-theory, ~a, ~a, ~a, ~a~%" test size density (- end-time start-time))))
+		 ((eq type :dependency-graph)
+		  (progn
+		    (setf start-time (get-internal-real-time))
+		    (simulate-rs filename #'simulator-dependency-graph "/dev/null")
+		    (setf end-time (get-internal-real-time))
+		    (format f "dependency-graph, ~a, ~a, ~a, ~a~%" test size density (- end-time start-time))))
+		 (t (error "unrecognized simulator")))
+	   (gc :full t)))))
 
 ;; example of how to perform all the tests
-;; (loop for n in '(10 100 1000 2000) do
+;; (loop for n in '(10 100 1000) do
 ;;      (loop for d in '(0.01 0.05 0.1) do
 ;; 	  (perform-tests n d "./test-models/" :set-theory "results.txt")
 ;; 	  (perform-tests n d "./test-models/" :dependency-graph "results.txt")))
